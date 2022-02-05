3196ca03f26606eb53415f40ec696e75030601b9
Rafael Castro <rafael.castro@dcc.ufmg.br>
2022-02-04 22:39:34 -0300

    Commit 4"

--- Customer.java
+++ Customer.java
@@ -27,7 +27,7 @@ public class Customer {
         Rental each = (Rental) rentals.nextElement();
 
         //determine amounts for each line
-        thisAmount = amountFor(each);
+        thisAmount = each.getCharge();
 
 
         // add frequent renter points
@@ -47,25 +47,5 @@ public class Customer {
      result += "You earned " + String.valueOf(frequentRenterPoints) +
              " frequent renter points";
      return result;
-   }
-   private double amountFor(Rental aRental) {
-      //Adicionar o trecho de código extraído.
-      double thisAmount = 0;
-      switch (each.getMovie().getPriceCode()) {
-         case Movie.REGULAR:
-            thisAmount += 2;
-            if (each.getDaysRented() > 2)
-               thisAmount += (each.getDaysRented() - 2) * 1.5;
-            break;
-         case Movie.NEW_RELEASE:
-            thisAmount += each.getDaysRented() * 3;
-            break;
-         case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (each.getDaysRented() > 3)
-               thisAmount += (each.getDaysRented() - 3) * 1.5;
-             break;
-      }
-      return thisAmount;
    }
 }
