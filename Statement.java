e56cfbefb9c009958b5dd09707b32842752766c3
Rafael Castro <rafael.castro@dcc.ufmg.br>
2022-02-04 23:45:52 -0300

    ccommit   18

--- Statement.java
+++ Statement.java
@@ -1,3 +1,13 @@
 public class Statement {
+  public String value(Customer aCustomer){
+      Enumeration rentals = aCustomer.getRentals();
+      String result = "";
+      while (rentals.hasMoreElements()) {
+         Rental each = (Rental) rentals.nextElement();
+         //show figures for this rental
+      }
+      //add footer lines
+      return result;
 
+  }
 }
--- TextStatement.java
+++ TextStatement.java
@@ -1,15 +1,5 @@
 import java.util.Enumeration;
 
 public class TextStatement extends Statement {
-  public String value(Customer aCustomer){
-      Enumeration rentals = aCustomer.getRentals();
-      String result = "";
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for this rental
-      }
-      //add footer lines
-      return result;
-
-  }
+  
 }
